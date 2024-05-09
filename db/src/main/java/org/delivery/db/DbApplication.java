0eae39c6ea84b9b091d134b8a489dc32e3dc62e5
이규훈 <dlrbgns0904@sju.ac.kr>
2024-05-09 21:58:35 +0900

    feat: db 모듈 추가

--- db/src/main/java/org/delivery/db/DbApplication.java
+++ db/src/main/java/org/delivery/db/DbApplication.java
@@ -1,15 +1,13 @@
-package org.delivery.api;
+package org.delivery.db;
 
-import org.delivery.db.UserDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class DbApplication {
 
     public static void main(String[] args) {
-        UserDto userDto = new UserDto();
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(DbApplication.class, args);
     }
 
 }
